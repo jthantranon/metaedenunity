a05b8d6bc4caade573f9bb2376148e090c7636d5
John Thantranon <john@uniconstudios.com>
2015-04-24 20:08:17 -0700

    Added Prefabs and network loading statue and wisp

--- Assets/Scripts/NetworkGameplay.cs
+++ Assets/Scripts/NetworkGameplay.cs
@@ -7,6 +7,10 @@ public class NetworkGameplay : MonoBehaviour {
 	private NetworkMessageHandler networkMessageHandler;
 
 	public GameObject wallPrefab;
+	public GameObject smallRockPrefab;
+	public GameObject teleporterPrefab;
+	public GameObject statuePrefab;
+	public GameObject wispPrefab;
 	public GameObject floor;
 
 	// Use this for initialization
@@ -14,6 +18,7 @@ public class NetworkGameplay : MonoBehaviour {
 		networkMessageHandler = GetComponent<NetworkMessageHandler>();
 		networkMessageHandler.CharacterInfo += OnCharacterInfo;
 		networkMessageHandler.ZoneInfo += OnZoneInfo;
+
 	}
 
 	// Update is called once per frame
@@ -40,24 +45,80 @@ public class NetworkGameplay : MonoBehaviour {
 		Debug.Log("Zone info received");
 		// TODO: set zone info { id: '', name: '', entities: [], walls: [], height: 0, width: 0 }
 		// TODO: add walls, add entities (except player)
-		var height = System.Convert.ToSingle(zoneInfo["height"]);
-		var width = System.Convert.ToSingle(zoneInfo["width"]);
-		floor.transform.localScale = new Vector3(width / 10f, 1, height /10f);
-		floor.transform.position = new Vector3(width / 2f, 0, height / 2f);
-		var walls = zoneInfo["walls"] as IEnumerable<object>;
-		foreach(IDictionary<string, object> wall in walls)
-		{
-			var startVector = ConvertToVector3(wall["start"]);
-			var endVector = ConvertToVector3(wall["end"]);
+		AdjustFloor (zoneInfo);
+		AddEntities (zoneInfo);
+		AddWalls (zoneInfo);
+	}
 
-			var middle = new Vector3((startVector.x + endVector.x) / 2f, 0, (startVector.z + endVector.z) / 2f);
-			var diff = new Vector3((startVector.x - endVector.x), 0, (startVector.z - endVector.z));
+	void AdjustFloor (IDictionary<string, object> zoneInfo)
+	{
+		var height = System.Convert.ToSingle (zoneInfo ["height"]);
+		var width = System.Convert.ToSingle (zoneInfo ["width"]);
+		floor.transform.localScale = new Vector3 (width / 10f, 1, height / 10f);
+		floor.transform.position = new Vector3 (width / 2f, 0, height / 2f);
+	}
+
+	void AddEntities (IDictionary<string, object> zoneInfo)
+	{
+		var entities = zoneInfo ["entities"] as IEnumerable<object>;
+		foreach (IDictionary<string, object> entity in entities) {
+			var type = entity ["type"] as string;
+			switch(type){
+			case "rock":
+				Debug.Log ("this is a rock");
+				break;
+			case "teleporter":
+				AddTeleporter(entity);
+				break;
+			case "statue":
+				AddStatue(entity);
+				break;
+			case "wisp":
+				AddWisp(entity);
+				break;
+			default:
+				Debug.LogWarning ("this is default:"+ type);
+				break;
+			}
+		}
+	}
+
+	void AddWisp (IDictionary<string,object> entity){
+		Debug.Log ("rendering Wisp");
+		var wispObject = (GameObject)Instantiate (statuePrefab);
+		var position = ConvertToVector3 (entity ["position"]);
+		wispObject.transform.position = position;
+	}
+
+	void AddStatue (IDictionary<string,object> entity){
+		Debug.Log ("rendering Statue");
+		var statueObject = (GameObject)Instantiate (statuePrefab);
+		var position = ConvertToVector3 (entity ["position"]);
+		statueObject.transform.position = position;
+	}
+
+	void AddTeleporter (IDictionary<string, object> entity){
+		Debug.Log ("rendering teleporter");
+		var teleporterObject = (GameObject)Instantiate (teleporterPrefab);
+		var position = ConvertToVector3 (entity ["position"]);
+		teleporterObject.transform.position = position;
+	}
+
+	void AddWalls (IDictionary<string, object> zoneInfo)
+	{
+		var walls = zoneInfo ["walls"] as IEnumerable<object>;
+		foreach (IDictionary<string, object> wall in walls) {
+			var startVector = ConvertToVector3 (wall ["start"]);
+			var endVector = ConvertToVector3 (wall ["end"]);
+			var middle = new Vector3 ((startVector.x + endVector.x) / 2f, 0, (startVector.z + endVector.z) / 2f);
+			var diff = new Vector3 ((startVector.x - endVector.x), 0, (startVector.z - endVector.z));
 			var magnitude = diff.magnitude;
-
-			var wallObject = (GameObject)Instantiate(wallPrefab, middle,Quaternion.identity);
+			var wallObject = (GameObject)Instantiate (wallPrefab, middle, Quaternion.identity);
 			wallObject.transform.position = middle;
-			wallObject.transform.localScale = new Vector3(magnitude, 1, 1);
-			wallObject.transform.Rotate(new Vector3(0, Mathf.Atan(diff.z / diff.x) * 180 / Mathf.PI, 0));
+			wallObject.transform.localScale = new Vector3 (magnitude, 1, 1);
+			wallObject.transform.Rotate (new Vector3 (0, Mathf.Atan (diff.z / diff.x) * 180 / Mathf.PI, 0));
+			// var meshRenderer = wallObject.GetComponent < MeshRenderer > ();
+			// meshRenderer.material.color = new Color(.5,.5,.5);
 		}
 	}
 
